910755ee93eee33dc60d013f27db53264e3dc857
Karanbir Singh <karanbir10092001@gmail.com>
2026-05-02 17:59:41 -0500

    removed comment

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatedd9xg5" # replace with your real storage acct
+    storage_account_name = "tfstatedd9xg5"
     container_name       = "tfstate"
     key                  = "network/terraform.tfstate"
   }
